8c46c4b5e4616bdefc22e579a314c5288db4da16
Artur Kosmatka <artur.kosmatka@gmail.com>
2021-10-16 14:54:58 +0100

    Adjust buildpacks on Heroku

--- project4/settings.py
+++ project4/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'static')
 
 MEDIA_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_URL = '/media/'
