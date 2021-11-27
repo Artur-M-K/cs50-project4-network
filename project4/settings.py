3e162e883d24a3d528c70324236e5563e53e38b2
Artur Kosmatka <artur.kosmatka@gmail.com>
2021-11-27 00:59:13 +0000

    change settings.py for heroku

--- project4/settings.py
+++ project4/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'project4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,17 +125,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'network/static'),
 )
+
+
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'network/static'),
+# )
 
 MEDIA_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_URL = '/media/'
